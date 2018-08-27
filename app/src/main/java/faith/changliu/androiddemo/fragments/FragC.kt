9d7632f8765f60c6b7449ba388740fab55c5a850
Chang Liu <bobbyliu117@gmail.com>
2018-08-26 21:05:38 -0400

    Fix: restore previous loaded data when back from FragA to FragC

--- app/src/main/java/faith/changliu/androiddemo/fragments/FragC.kt
+++ app/src/main/java/faith/changliu/androiddemo/fragments/FragC.kt
@@ -14,34 +14,31 @@ import faith.changliu.androiddemo.helpers.getContentFromStringUrl
 import faith.changliu.androiddemo.helpers.isConnected
 import faith.changliu.androiddemo.helpers.snackbar
 import kotlinx.android.synthetic.main.frag_c.*
-import java.io.IOException
-import java.net.MalformedURLException
-import java.net.URL
 import java.util.concurrent.Executors
 
 class FragC : Fragment() {
 
+	// keep track if data is loaded
+	private var mTest: Test? = null
+
 	// singleton
 	companion object {
-		val instance by lazy {
-			FragC()
-		}
+		val instance by lazy { FragC() }
 	}
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		if (instance.view != null) return instance.view
 		return inflater.inflate(R.layout.frag_c, container, false)
-	}
-
-	override fun onDestroyView() {
-		super.onDestroyView()
-		println("changerror-FragC destroyed")
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		mBtnGetData.setOnClickListener {
-			_ ->
+
+		// bind view if data exists
+		mTest?.let {
+			bindTestDataWithCardView(it)
+		}
+
+		mBtnGetData.setOnClickListener { _ ->
 			if (isConnected()) loadTest()
 			else view.snackbar(R.string.no_internet)
 		}
@@ -62,9 +59,14 @@ class FragC : Fragment() {
 			activity?.runOnUiThread {
 
 				if (dataString.isNotEmpty()) {
+					// convert json to Test instance
 					val tests = Gson().fromJson(dataString, TestDataProvider::class.java)
+					// check if Test instance exists
 					if (tests.data.isNotEmpty()) {
-						bindTestDataWithCardView(tests.data[0])
+						tests.data[0].let {
+							mTest = it
+							bindTestDataWithCardView(it)
+						}
 					}
 				}
 
